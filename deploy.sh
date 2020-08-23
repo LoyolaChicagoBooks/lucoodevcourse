27b41802f953da2b474752ba3b0a89d2cfe68140
Konstantin Läufer <klaeufer@gmail.com>
2020-08-23 13:28:13 -0500

    made configuration consistent with PL notes
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEST=../lucoodevcourse.github.io
+
+rm -rf $DEST/*
+touch $DEST/.nojekyll
+mkdir -p $DEST/download
+cp -a build/html/* $DEST/
+cp build/latex/*.pdf $DEST/download/
+cp build/epub/*.epub $DEST/download/oodev.epub
+
+git log master --pretty=short --abbrev-commit > $DEST/COMMITS.txt
+
+pushd $DEST
+git add -A
+git commit -m "Generated site for `git log master -1 --pretty=short --abbrev-commit`" && git push
+popd
