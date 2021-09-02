4967e1ed934f9da708c20ff699e440196f5d6ca5
Konstantin Läufer <klaeufer@gmail.com>
2021-09-02 15:38:34 -0500

    updated deploy script
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cp -a build/html/* $DEST/
 cp build/latex/*.pdf $DEST/download/
 cp build/epub/*.epub $DEST/download/oodev.epub
 
-git log master --pretty=short --abbrev-commit > $DEST/COMMITS.txt
+git log main --pretty=short --abbrev-commit > $DEST/COMMITS.txt
 
 pushd $DEST
 git add -A
